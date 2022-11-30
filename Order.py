e330562e61ffb7697c0313f38705e201f83e1e98
Brian Nguyen <briannguyen9101@gmail.com>
2022-11-30 00:04:03 -0600

    Worked on Order class

--- Order.py
+++ Order.py
@@ -1,12 +1,46 @@
-import Item
-import Inventory
 class Order:
-    def __init__(self):
-        self.ID = -1
+    def __init__(self, db, cursor):
+        self.db = db
+        self.cursor = cursor
 
-    def addItem(self, itemID, quantity, price):
-        return
+    # Generate orderID
+    def _genOrderID(self, userID):
+        query = "SELECT MAX(orderID) FROM orders WHERE userID=%i"
+        data = (userID,)
+        self.cursor.execute(query, data)
+        result = self.cursor.fetchone()
+        
+        return result[0]+1 if result != None else False
 
-    def setDateTime(self, dateTime):
-        return
-
+    # Add an entry to user's order history
+    def addEntry(self, userID, items:tuple):
+        query = "INSERT INTO orders VALUES(%i, %i, %i, %s, %i, %i, %s)"
+        data = (items[0], items[1], userID, items[3], items[4], items[5], items[6])
+        
+        self.cursor.execute(query, data)
+        self.db.commit()
+    
+    # View an entry from user's order history
+    def viewEntry(self, userID, orderID):
+        query = "SELECT * FROM orders WHERE userID=%i AND orderID=%i"
+        data = (userID, orderID,)
+        self.cursor.execute(query, data)
+        result = self.cursor.fetchone()
+        
+        return result if result != None else False
+        
+    # Clear user's entire order history
+    def clearHistory(self, userID):
+        query = "DELETE * FROM orders WHERE userID=%i"
+        data = (userID,)
+        self.cursor.execute(query, data)
+        self.db.commit()
+    
+    # View user's entire order history
+    def viewHistory(self, userID):
+        query = "SELECT * FROM orders WHERE userID=%i"
+        data = (userID,)
+        self.cursor.execute(query, data)
+        results = self.cursor.fetchall()
+        
+        return results
